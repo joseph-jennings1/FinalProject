9ada6724354ba2ae1f71c39d343865d3d6ff7158
joseph-jennings1 <40963397+joseph-jennings1@users.noreply.github.com>
2018-08-14 19:52:19 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,27 @@
-
+function ValidateForm() {
+  var validName = false; 
+  var validEmail = false; 
+  var validComment = false; 
+  
+   if (myContact.name.value===null ||
+ myContact.name.value==="")
+ errorMessages += "<p>A contact name is required.</p>";
+  else
+ validName =true;
+  
+   if (myContact.email.value===null ||
+ myContact.email.value==="")
+ errorMessages += "<p>An email is required.</p>";
+  else
+ validEmail =true;
+  
+   if (myContact.comment.value===null ||
+ myContact.comment.value==="")
+ errorMessages += "<p>A comment is required.</p>";
+  else
+ validComment =true;
+ 
+  document.getElementById("errorMessages").innerHTML = errorMessages;
+// Make sure you return all the boolean variables that are checking each field
+ return (validName && validEmail && validComment);
+ }
